5d3c9809b7a5628edec6360ec4278a972727d3e6
Michael Law <michael.law@ontariotechu.net>
2021-03-16 22:23:12 -0400

    Start + Singleplayer Chat

---
+++ Midterm_Client/Assets/Scripts/cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cube : MonoBehaviour
+{
+    public InputField chatBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!chatBox.isFocused)
+        {
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 2f,
+                                0, Input.GetAxis("Vertical") * Time.deltaTime * 2f);
+        }
+
+    }
+}
